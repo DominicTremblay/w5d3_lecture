4248f772e98df4bf6ca56eb2a69029e79fb4c02b
Dominic Tremblay <dominictremblay24@gmail.com>
2020-09-16 09:50:45 -0400

    demo reset

--- app.js
+++ app.js
@@ -3,13 +3,6 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const db = require('./db');
-const {
-  getActors,
-  getActorById,
-  getActorsByMovie,
-  addNewActor,
-} = require('./helpers/dbHelpers')(db);
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -29,34 +22,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.get('/actors', (req, res) => {
-  const { movie } = req.query;
-
-  if (!movie) {
-    getActors()
-      .then((actors) => res.json(actors))
-      .catch((err) => res.send(err));
-  } else {
-    getActorsByMovie(movie)
-      .then((actors) => res.json(actors))
-      .catch((err) => res.send(err));
-  }
-});
-
-app.get('/actors/:id', (req, res) => {
-  const { id } = req.params;
-  getActorById(id)
-    .then((actor) => res.json(actor))
-    .catch((err) => res.json({ err: err.message }));
-});
-
-app.post('/actors', (req, res) => {
-  const actor = req.body;
-
-  addNewActor(actor)
-    .then((actor) => res.json(actor))
-    .catch((err) => res.json({ err }));
-});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
